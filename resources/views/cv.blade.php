d1f8350e2185902a805b4eee739af5a9f820b6f3
JacksonMingz <jacksonmingz@gmail.com>
2023-03-29 08:51:17 +0700

    done

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -24,20 +24,23 @@
           <div class="collapse navbar-collapse justify-content-lg-end" id="navbarNav">
             <ul class="navbar-nav mb-2 mb-lg-0 align-items-center">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#home">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="#about">About Me</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
+                <a class="nav-link" href="#skill">Skills</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact</a>
               </li>
             </ul>
           </div>
         </nav>
       </div>
 
-      <div class="row">
+      <div id="home" class="row">
         <div class="col-12">
           <div class="position-relative">  
             <img class="image" src="{{ asset('bg2.jpg') }}">
@@ -79,16 +82,394 @@
           </div>
         </div>
 
-        <div class="col-12">
+        <div id="about" class="col-12 pt-5">
           <div class="container">
             <div class="row justify-content-center">
-              <div class="col-md-10 offset-md-2">
-                Test HALOOOO
+              <div class="col-md-8 offset-md-1 border">
+                <h1 class="head">P E K E R J A A N</h1>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h3 class="subhead">TEKNISI</h3>
+                      <h5 class="content"><b>2022 December - Sekarang</b></h5>
+                      <p class="content">PT Sat Nusapersada Tbk</p>
+                    </div>
+                </div>
               </div>
             </div>
+
+            <div class="row justify-content-center mt-5">
+              <div class="col-md-8 offset-md-1 border">
+                <h1 class="head">P E N D I D I K A N</h1>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 class="subhead">S1 - System Information</h3>
+                      <h5 class="content"><b>2022 - Sekarang</b></h5>
+                      <p class="content">Universitas Internasional Batam</p>
+                    </div>
+                    <div class="col-md-6">
+                      <h3 class="subhead">SMA - Science Major</h3>
+                      <h5 class="content"><b>2019 - 2022</b></h5>
+                      <p class="content">SMA Kristen Kalam Kudus Selatpanjang</p>
+                    </div>
+                </div>
+              </div>
+            </div>            
+
+            <div class="row justify-content-center mt-5">
+              <div class="col-md-8 offset-md-1 border">
+                <h1 class="head">P E N G H A R G A A N</h1>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 class="subhead">Juara 1 PKM Creativity and Innovation Program</h3>
+                      <h5 class="content"><b>Universitas Internasional Batam | 2022</b></h5>
+                      <p class="content">~ Berhasil menyelesaikan PKM dalam kurang lebih sebulan dengan membuat simulasi aplikasi untuk membantu pedagang kaki lima.</p>
+                    </div>
+                    <div class="col-md-6">
+                      <h3 class="subhead">The Best Physics</h3>
+                      <h5 class="content"><b>SMA Kristen Kalam Kudus Selatpanjang | 2019 - 2022</b></h5>
+                      <p class="content">~ Berhasil mendapatkan nilai fisika tertinggi selama 6 Semester</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row justify-content-center mt-5">
+              <div class="col-md-8 offset-md-1 border">
+                <h1 class="head">O R G A N I S A S I</h1>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h3 class="subhead">Vice Presidents of Student Council</h3>
+                      <h5 class="content"><b>SMA Kristen Kalam Kudus Selatpanjang | 2020 - 2021</b></h5>
+                      <p class="content">~ Berperan dalam mengatur jalannya kegiatan OSIS, dan mengatur anggota OSIS lainnya.</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div id="skill" class="row justify-content-center mt-5">
+              <div class="col-md-8 offset-md-1 border">
+                <h1 class="head">S K I L L S</h1>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 class="subhead">Software</h3>
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 90%;">
+                              <span class="ms-2 align-middle">
+                                Ms. Office
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 90% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 87%;">
+                              <span class="ms-2 align-middle">
+                                Photoshop
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 87% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 86%;">
+                              <span class="ms-2 align-middle">
+                                Figma
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 86% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 85%;">
+                              <span class="ms-2 align-middle">
+                                After Effects
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 85% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                    </div>
+                    <div class="col-md-6">
+                      <h3 class="subhead">Skills</h3>
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 94%;">
+                              <span class="ms-2 align-middle">
+                                PHP Language
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 94% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 90%;">
+                              <span class="ms-2 align-middle">
+                                C Language
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 90% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 88%;">
+                              <span class="ms-2 align-middle">
+                                HTML | CSS | Js
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 88% </b>
+                          </span>
+                        </div>
+                      </div>
+                      
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 87%;">
+                              <span class="ms-2 align-middle">
+                                Query
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 87% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 class="subhead">Language</h3>
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 96%;">
+                              <span class="ms-2 align-middle">
+                                Indonesia | Active
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 96% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 90%;">
+                              <span class="ms-2 align-middle">
+                                English | Active
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 90% </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 90%;">
+                              <span class="ms-2 align-middle">
+                                Hokkien | Active
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 90% </b>
+                          </span>
+                        </div>
+                      </div>
+                      
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad" style="width: 53%;">
+                              <span class="ms-2 align-middle">
+                                Mandarin | Passive
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> 53% </b>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <h3 class="subhead">Hobby</h3>
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad">
+                              <span class="ms-2 align-middle">
+                                Digital Drawing
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> &nbsp; </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad">
+                              <span class="ms-2 align-middle">
+                                Coding
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> &nbsp; </b>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad">
+                              <span class="ms-2 align-middle">
+                                Solving Math
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> &nbsp; </b>
+                          </span>
+                        </div>
+                      </div>
+                      
+                      <div class="row mb-2">
+                        <div class="wrapper col-9 justify-content-center pe-0">
+                          <div class="backLoad">
+                            <div class="theLoad">
+                              <span class="ms-2 align-middle">
+                                Design
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-3 percent ps-0">
+                          <span class="ms-2 align-middle">
+                            <b> &nbsp; </b>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
           </div>
         </div>
-      </div>
+
+<footer id="contact" class="text-center text-white mt-5" style="background-color: #f1f1f1;">
+  <div class="container pt-4">
+    <section class="mb-4">
+
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="instagram.com/jacksonmingz"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="bi bi-instagram"></i
+      ></a>
+
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://github.com/JacksonMingz/CV"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="bi bi-github"></i
+      ></a>
+    </section>
+  </div>
+
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-dark">JacksonMingz</a>
+  </div>
+</footer>
       
 </body>
 </html>
